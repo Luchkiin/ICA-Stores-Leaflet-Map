bebe2fe4acaebdc141a54081bd6095367c0048b5
Lucas Rosendahl <34438601+Luchkiin@users.noreply.github.com>
2019-12-01 20:51:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-//skapar upp en ny map med hjälp av L.map som hämtas från Leaflet biblioteket
-//Sätter vyn med koordinater för longitud och latitud
 var map = L.map('mapid').setView([60.59055, 15.61365,], 8);
-//Lägger på ett tileLayer (dvs, kartlager)
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibHVjaGtpaW4iLCJhIjoiY2p2aHcydDUzMDdrNDN5czFhN2xiY3h3cyJ9.8c9MPBQdQAXt28iPRwH9Zw', {
     maxZoom: 14,
-    //Ger cred till api som vi har använts oss utav 
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -12,7 +8,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 }).addTo(map);
 L.control.scale().addTo(map);
 
-//Foreach som loopar genom alla features i JSON-filen för att komma åt samtliga properties 
 function myForEachFeature(feature, layer) {
     if (feature.properties) {
         popmsg = "<h3><b>Store </b></h3>" + feature.properties.store + "<br><br>";
@@ -20,37 +15,28 @@ function myForEachFeature(feature, layer) {
         popmsg += "<h3><b>Address </b></h3>" + feature.properties.adress + "<br>";
         popmsg += "<h3><b>Opening hours </b></h3>" + feature.properties.openingHours + "<br>";
         popmsg += "<h3><b>Webpage </b></h3><a id=websiteLink target=_blank href=" + feature.properties.website + ">" + "Click here</a> <i class='fas fa-caret-square-right'></i><br>";
-        //binder upp i ett Popup-fönster
         layer.bindPopup(popmsg);
     }
 };
 
-//funktion som skapar listan för alla våra våra butiker. Presenteras via en lista I HTML-dokumentet
 function sideList(jData) {
-    //for-loop. Loopar för varje varv i loopen som skapar divar och appendar butikerna
     for (i = 0; jData.features.length; i++) {
         let currFeature = jData.features[i];
         let prop = currFeature.properties;
-        //hämtar listingselemenet via getElementById('listings') 
         let listings = document.getElementById('listings');
-        //Appendar nytt div-element 
         let listing = listings.appendChild(document.createElement('div'));
         listing.className = 'item';
         listing.id = 'listing ' + i;
-
-        //titel på butiken samt adress som en link i listan
         let link = listing.appendChild(document.createElement('a'));
         link.href = '#';
         link.className = 'title';
         link.dataPosition = i;
         link.innerHTML = "<h3>" + prop.store + ", " + prop.adress + "</h3>";
 
-        //övrig info angående butikerna 
         let info = listings.appendChild(document.createElement('div'));
         info.className = 'info';
         info.innerHTML = prop.openingHours;
 
-        //event som skickar överensstämmande butiksdata via onclick till funktionen swap element
         link.addEventListener("click", function () {
             let item = jData.features[this.dataPosition];
             swapElements(item);
@@ -58,76 +44,53 @@ function sideList(jData) {
     }
 }
 
-//i GEOJSON är longitud före latitud, så vi byter positioner med varandra för att få flyTo att funka 
 function swapElements(item) {
     let temp = item.geometry.coordinates[0];
     item.geometry.coordinates[0] = item.geometry.coordinates[1];
     item.geometry.coordinates[1] = temp;
-    console.log(item.geometry.coordinates);
-    //Efter position byte skickar vi vidare datat till en annan funktion 
     highlightStore(item);
 }
 
-//centrerar kartan till den klickade butikens position 
 function highlightStore(clickedItem) {
-    //Zoomar in till nivå 14 för den klickade butikens position via map.flyTo
     map.flyTo(clickedItem.geometry.coordinates, 13);
 }
 
-//onclick som hämtar data från två externa leverantörer baserat på koordinater
 map.on("click", event => {
     let latlngObj = event.latlng;
     let lat = latlngObj.lat.toFixed(1);
     let lng = latlngObj.lng.toFixed(1);
-    console.log(latlngObj);
 
     let url = "https://api.resrobot.se/v2/location.nearbystops?key=bc972fe4-be45-43ad-84dc-a52c31eee54e&originCoordLat=" + lat + "&originCoordLong=" + lng + "&format=json";
     let url1 = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&units=metric&appid=25f4530d6bd98eb444ce6b94f8db1ef8";
 
-    //$.when hämtar data från två urls samtidigt
     $.when($.getJSON(url), $.getJSON(url1)).done(function (buss, weather) {
         let data = [buss, weather];
-        console.log(buss[0].StopLocation);
-        console.log(weather);
-        //skickar sedan detta till en annan funktion
         createBusstops(latlngObj, data);
     });
-    // $.getJSON(url1, function(data){
-    //     createWeatherPopUp(latlngObj,data);
-    // })
-
 });
 
 
-//Funktion för att skapa upp närliggande busshållplatser i förhållande till vald marker (affär) för busshållplatsinfo
 function createBusstops(latlngObj, data) {
     let buss = data[0];
     let weather = data[1];
-    //loopar genom varje busshållsplats och skapar markers och popups för de, samt för vädret 
     $.each(buss[0].StopLocation, function (idx, element) {
         let marker = L.marker([element.lat, element.lon]).addTo(map);
         let popup = L.popup();
         popup.setLatLng(latlngObj);
-        //skapar upp väder-Popup:et
         let tempMsg = "<img src='https://openweathermap.org/img/wn/" + weather[0].weather[0].icon + ".png' class='weather-icon-img'>" + "<h3 class='info-heading-h3'>Current weather" + "<br>" + "in " + weather[0].name + "</h3>";
         tempMsg += "<ul><li><b>Temp: </b>" + weather[0].main.temp + " &#8451</li>" + "<li><b>Condition: </b>" + weather[0].weather[0].description + "</li>" + "<li><b>Humidity: </b>" + weather[0].main.humidity + "%" + "</li>" + "<li><b>Wind: </b>" + weather[0].wind.speed + " m/s" + "</li>" + "</ul>";
-        //Sätter content
         popup.setContent(tempMsg);
         popup.openOn(map);
-        //Ett click event som hämtar anländningstiden gällande för busshållplats 
         marker.on('click', function (e) {
-            let deptTime = "";
-            //Hämtar busstider för för busshållsplats via ID
-            $.getJSON("https://api.resrobot.se/v2/departureBoard?key=3a4b0582-ef3d-4adc-8e40-5167a782d122&id=" + element.id + "&maxJourneys=1&format=json", function (data) {
-                //loopar genom datat baserat på ID som vi hämtar när vi klickar på en buss-marker och lägger det i en popup
-                $.each(data, function (idx, item) {
-                    console.log(data);
-                    deptTime = data.Departure[0].Stops.Stop[0].depTime;
-                    busstopName = data.Departure[0].Stops.Stop[0].name;
-                    console.log(data.Departure[0].name);
-                    console.log(item);
+            let url1 = "https://api.resrobot.se/v2/departureBoard?key=3a4b0582-ef3d-4adc-8e40-5167a782d122&id=";
+            let departureTime = "";
+            $.getJSON(url1 + element.id + "&maxJourneys=1&format=json", function (data) {
+                $.each(data, function () {
+                    busStopName = data.Departure[0].Stops.Stop[0].name;
+                    departureDate = data.Departure[0].Stops.Stop[0].depDate;
+                    departureTime = data.Departure[0].Stops.Stop[0].depTime;
                     let busMsg = "<img src='img/school-bus.png' class='school-bus-img'>" + "<h3 class='info-heading-h3'>Next bus</h3>";
-                    busMsg += "<ul>" + "<li><b>From: </b>" + busstopName + "</li>" + "<b><li>Departs: </b>" + deptTime + "</li></ul>";
+                    busMsg += "<ul><li><b>From: </b>" + busStopName + "<li><b>Date: </b>" + departureDate + "</li>" + "</li>" + "<b><li>Departure time: </b>" + departureTime + "</li></ul>";
                     popup.setContent(busMsg);
                     popup.openOn(map);
                 });
@@ -136,16 +99,13 @@ function createBusstops(latlngObj, data) {
     });
 }
 
-//hämtar JSON-fil för ICA-affärerna
 let jsonfile = "json/icaStores.json";
 
-//Skapar upp en egenvald ikon för affärerna
 let customIcon = L.icon({
     iconUrl: 'img/ica-marker-icon.png',
     iconSize: [40, 40]
 });
 
-//hämtar jsondata och skickar de till andra funktioner, samt stylar butikmarkers 
 $.getJSON(jsonfile, function (jsondata) {
     L.geoJSON(jsondata, {
         onEachFeature: myForEachFeature,
