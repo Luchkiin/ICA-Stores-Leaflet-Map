4a4d1e3c4807da3384db3da01441f4ac5f2bb4db
Lucas <lr@lucasrosendahl.com>
2020-07-07 09:59:36 +0200

    new updates

--- js/script.js
+++ js/script.js
@@ -10,39 +10,55 @@ L.control.scale().addTo(map);
 
 function myForEachFeature(feature, layer) {
     if (feature.properties) {
-        popmsg = "<h3><b>Store </b></h3>" + feature.properties.store + "<br><br>";
-        popmsg += "<img id=websiteImg src=" + feature.properties.picURL + " width=100>";
-        popmsg += "<h3><b>Address </b></h3>" + feature.properties.adress + "<br>";
-        popmsg += "<h3><b>Opening hours </b></h3>" + feature.properties.openingHours + "<br>";
-        popmsg += "<h3><b>Webpage </b></h3><a id=websiteLink target=_blank href=" + feature.properties.website + ">" + "Click here</a> <i class='fas fa-caret-square-right'></i><br>";
+        popmsg = "<div class='store-image-container'><img id=websiteImg class=store-image src=" + feature.properties.picURL + " width=100><span>" + feature.properties.store + "</span></div>";
+        popmsg += "<div class='store-info-wrapper'><div class='store-heading-container'><h3>Store</h3><span>" + feature.properties.store + "</span></div>";
+        popmsg += "<div class='store-heading-container'><h3>Address</h3><span>" + feature.properties.adress + "</span></div>";
+        popmsg += "<div class='store-heading-container'><h3>Hours</h3><span>" + feature.properties.openingHours + "</span></div>";
+        popmsg += "<div class='store-heading-container'><h3>Webpage</h3><a id=websiteLink class=store-website-link target=_blank href=" + feature.properties.website + ">" + "Click here</a><i class='fas fa-caret-square-right'></i></div></div>";
         layer.bindPopup(popmsg);
     }
 };
 
 function sideList(jData) {
+
     for (i = 0; jData.features.length; i++) {
         let currFeature = jData.features[i];
         let prop = currFeature.properties;
-        let listings = document.getElementById('listings');
+        let listings = document.getElementById('sidebar');
         let listing = listings.appendChild(document.createElement('div'));
-        listing.className = 'item';
-        listing.id = 'listing ' + i;
-        let link = listing.appendChild(document.createElement('a'));
-        link.href = '#';
-        link.className = 'title';
-        link.dataPosition = i;
-        link.innerHTML = "<h3>" + prop.store + "</h3>";
+        listing.className = 'sidebar-accordion ' + i;
+        listing.id = 'sidebar-accordion ' + i;
 
-        let info = listings.appendChild(document.createElement('div'));
-        info.className = 'info';
-        info.innerHTML = prop.openingHours;
+        let linkSideBarTitle = listing.appendChild(document.createElement('a'));
+        linkSideBarTitle.href = '#';
+        linkSideBarTitle.className = 'sidebar-title ' + i;
+        linkSideBarTitle.dataPosition = i;
+        linkSideBarTitle.innerHTML = "<h3>" + prop.store + "</h3>";
 
-        link.addEventListener("click", function () {
+        let linkAccordion = listing.appendChild(document.createElement('a'));
+        linkAccordion.href = '#';
+        linkAccordion.className = 'accordion-icon ' + i;
+        linkAccordion.dataPosition = i;
+
+        let info = listing.appendChild(document.createElement('div'));
+        info.className = 'accordion-info';
+        info.innerHTML = "<div class='accordion-info-container'><div><h4>Hours</h4><span>" + prop.openingHours + "</span></div><div><>Address<span>" + prop.adress + "</span></div></div>";
+
+        let items = document.querySelectorAll(".sidebar-accordion .accordion-icon");
+        items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+        linkSideBarTitle.addEventListener("click", function () {
             let item = jData.features[this.dataPosition];
             swapElements(item);
         });
     }
 }
+
+function toggleAccordion() {
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('active');
+}
+
 
 function swapElements(item) {
     let temp = item.geometry.coordinates[0];
@@ -74,11 +90,11 @@ function createBusstops(latlngObj, data) {
     let buss = data[0];
     let weather = data[1];
     $.each(buss[0].StopLocation, function (idx, element) {
-        let marker = L.marker([element.lat, element.lon]).addTo(map);
+        let marker = L.marker([element.lat, element.lon], { icon: customBusIcon }).addTo(map);
         let popup = L.popup();
         popup.setLatLng(latlngObj);
-        let tempMsg = "<img src='https://openweathermap.org/img/wn/" + weather[0].weather[0].icon + ".png' class='weather-icon-img'>" + "<h3 class='info-heading-h3'>Current weather" + "<br>" + "in " + weather[0].name + "</h3>";
-        tempMsg += "<ul><li><b>Temp: </b>" + weather[0].main.temp + " &#8451</li>" + "<li><b>Condition: </b>" + weather[0].weather[0].description + "</li>" + "<li><b>Humidity: </b>" + weather[0].main.humidity + "%" + "</li>" + "<li><b>Wind: </b>" + weather[0].wind.speed + " m/s" + "</li>" + "</ul>";
+        let tempMsg = "<div class='weather-image-container'><img src='https://openweathermap.org/img/wn/" + weather[0].weather[0].icon + ".png' class='weather-icon-img'><h3 class='info-heading-h3'>" + weather[0].name + "</h3></div>";
+        tempMsg += "<div class='weather-info-wrapper'><div class='weather-heading-container'><h3>Temperature</h3><span>" + weather[0].main.temp.toFixed(1) + " &#8451;</span></div><div class='weather-heading-container'><h3>Condition</h3><span class='capitalize'>" + weather[0].weather[0].description + "</span></div><div class='weather-heading-container'><h3>Humidity</h3><span>" + weather[0].main.humidity + " %</span></div><div class='weather-heading-container'><h3>Wind</h3><span>" + weather[0].wind.speed + " m/s</span></div></div>";
         popup.setContent(tempMsg);
         popup.openOn(map);
         marker.on('click', function (e) {
@@ -89,8 +105,8 @@ function createBusstops(latlngObj, data) {
                     busStopName = data.Departure[0].Stops.Stop[0].name;
                     departureDate = data.Departure[0].Stops.Stop[0].depDate;
                     departureTime = data.Departure[0].Stops.Stop[0].depTime;
-                    let busMsg = "<img src='img/school-bus.png' class='school-bus-img'>" + "<h3 class='info-heading-h3'>Next bus</h3>";
-                    busMsg += "<ul><li><b>From: </b>" + busStopName + "<li><b>Date: </b>" + departureDate + "</li>" + "</li>" + "<b><li>Departure time: </b>" + departureTime + "</li></ul>";
+                    let busMsg = "<div class='bus-image-container'><img src='img/bus-icon-big.png' class='bus-img'><h3 class='info-heading-h3'>Next bus</h3></div>";
+                    busMsg += "<div class='bus-info-wrapper'><div class='bus-heading-container'><h3>From</h3><span>" + busStopName + "</span></div><div class='bus-heading-container'><h3>Date</h3><span>" + departureDate + "</span></div><div class='bus-heading-container'><h3>Departure time</h3><span>" + departureTime + "</span></div></div>";
                     popup.setContent(busMsg);
                     popup.openOn(map);
                 });
@@ -99,25 +115,32 @@ function createBusstops(latlngObj, data) {
     });
 }
 
-let jsonfile = "json/icaStores.json";
+var jsonFile = "json/icaStores.json";
 
-let customIcon = L.icon({
+//Custom Marker Icons
+var customStoreIcon = L.icon({
     iconUrl: 'img/ica-marker-icon.png',
-    iconSize: [40, 40]
+    iconSize: [40],
+    popupAnchor:  [0, 15]
 });
 
-$.getJSON(jsonfile, function (jsondata) {
-    L.geoJSON(jsondata, {
+var customBusIcon = L.icon({
+    iconUrl: 'img/bus-marker-icon.png',
+    iconSize: [28]
+});
+
+$.getJSON(jsonFile, function (jsonData) {
+    L.geoJSON(jsonData, {
         onEachFeature: myForEachFeature,
         pointToLayer: function (feature, latlng) {
-            return L.marker(latlng, { icon: customIcon });
+            return L.marker(latlng, { icon: customStoreIcon }, {icon: customBusIcon});
         }
     }).addTo(map);
-    sideList(jsondata);
+    sideList(jsonData);
 })
 
 
-/*HAMBURGER MENU*/ 
+/*HAMBURGER MENU*/
 const navSlide = () => {
     const burgerMenu = document.querySelector('.burger-menu');
     const nav = document.querySelector('.nav-links');
