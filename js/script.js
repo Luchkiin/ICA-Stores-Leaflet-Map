3b36bddc945e74b919bd0fc2b785cb9d0618c66d
Lucas Rosendahl <lr@lucasrosendahl.com>
2020-11-26 14:30:25 +0100

    fixed small css bugs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 var map = L.map('mapid').setView([60.59055, 15.61365,], 8);
-
-
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
 tileSize: 512,
